357d0d05ae3a6446676827cf25d1e58863491b2d
pr862 <princyvithani09@gmail.com>
2025-07-10 12:27:30 +0000

    changes

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -1,16 +1,14 @@
 import React, {useContext} from "react";
-import "./WorkExperience.scss";
+import "./WorkExperience.scss";  // Make sure this SCSS file exists
 import ExperienceCard from "../../components/experienceCard/ExperienceCard";
 import {workExperiences} from "../../portfolio";
-<<<<<<< HEAD
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
-=======
-import "./WorkExperience.scss"; // Make sure this SCSS file exists
->>>>>>> origin/master
 
 export default function WorkExperience() {
   const {isDark} = useContext(StyleContext);
+
+  // Check if the 'display' property in workExperiences is true
   if (workExperiences.display) {
     return (
       <div id="experience">
@@ -19,20 +17,21 @@ export default function WorkExperience() {
             <div>
               <h1 className="experience-heading">Experiences</h1>
               <div className="experience-cards-div">
+                {/* Loop through all experiences */}
                 {workExperiences.experience.map((card, i) => {
                   return (
                     <ExperienceCard
-  isDark={isDark}
-  cardInfo={{
-    company: workExperiences.experience[0].company,
-    desc: workExperiences.experience[0].desc,
-    date: workExperiences.experience[0].date,
-    companylogo: workExperiences.experience[0].companylogo,
-    role: workExperiences.experience[0].role,
-    descBullets: workExperiences.experience[0].descBullets
-  }}
-/>
-
+                      key={i}  // Add a unique key for each element in the list
+                      isDark={isDark}
+                      cardInfo={{
+                        company: card.company,
+                        desc: card.desc,
+                        date: card.date,
+                        companylogo: card.companylogo,
+                        role: card.role,
+                        descBullets: card.descBullets
+                      }}
+                    />
                   );
                 })}
               </div>
@@ -42,5 +41,6 @@ export default function WorkExperience() {
       </div>
     );
   }
-  return null;
+
+  return null;  // If workExperiences.display is false, return null (no content rendered)
 }
