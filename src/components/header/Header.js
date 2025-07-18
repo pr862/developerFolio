4dee45252a81b38979956fe35a837356cced73f8
pr862 <princyvithani09@gmail.com>
2025-07-18 13:24:45 +0530

    add logo name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -22,6 +22,7 @@ function Header() {
       <header className={isDark ? "dark-menu header" : "header"}>
         <a href="" className="logo">
           <span className="grey-color"> &lt;</span>princy vithani
+          <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
